fe410bb573be2a5eb9696ba7ef2f40d17e99d741
Senior Dev <seniordev1008@gmail.com>
2021-05-27 13:41:52 -0400

    Initial router handler

--- instances/infrastructures/commands/microservice.go
+++ instances/infrastructures/commands/microservice.go
@@ -1,8 +1,12 @@
 package instances
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
+
+	"bitbucket.org/burhanmubarok/microservice/structures/https"
+	"github.com/gorilla/mux"
 )
 
 // Microservice doc
@@ -37,8 +41,53 @@ func (c *Microservice) Migrate() {
 // Serve doc
 func (c *Microservice) Serve() {
 	c.figlet()
+
+	router := mux.NewRouter().StrictSlash(true)
+
+	router.NotFoundHandler = http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusNotFound)
+		data := structures.Response{
+			Code:    http.StatusNotFound,
+			Message: "URL not found",
+		}
+		json.NewEncoder(w).Encode(data)
+	})
+
+	router.MethodNotAllowedHandler = http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusMethodNotAllowed)
+		data := structures.Response{
+			Code:    http.StatusMethodNotAllowed,
+			Message: "Method not allowed",
+		}
+		json.NewEncoder(w).Encode(data)
+	})
+
+	router.Path("/").Methods("GET").HandlerFunc(func(res http.ResponseWriter, req *http.Request) {
+		res.Header().Set("Content-Type", "application/json")
+		res.WriteHeader(http.StatusOK)
+		data := structures.Response{
+			Code:    http.StatusOK,
+			Message: "Wellcome! trust me, you are a coder",
+		}
+		json.NewEncoder(res).Encode(data)
+	})
+
+	router.Path("/about").Methods("GET").HandlerFunc(func(res http.ResponseWriter, req *http.Request) {
+		res.Header().Set("Content-Type", "application/json")
+		res.WriteHeader(http.StatusOK)
+		data := structures.Response{
+			Code:    http.StatusOK,
+			Message: "Microservice [desc]",
+		}
+		json.NewEncoder(res).Encode(data)
+	})
+
+	handlers := router
 	server := &http.Server{
-		Addr: ":8008",
+		Addr:    ":8008",
+		Handler: handlers,
 	}
 
 	serverError := server.ListenAndServe()
