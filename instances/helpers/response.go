3ac638363670cf475bd1e874025b1fcb577ef84e
Senior Dev <seniordev1008@gmail.com>
2021-05-30 04:07:37 -0400

    change log severity to debug

--- instances/helpers/response.go
+++ instances/helpers/response.go
@@ -41,6 +41,6 @@ func (r *Response) send(w http.ResponseWriter, httpStatus int, data structures.R
 		Errors:  data.Errors,
 	}
 	log := &instances.Logger{}
-	log.Info(logStruct.Log{Data: responseData})
+	log.Debug(logStruct.Log{Data: responseData})
 	json.NewEncoder(w).Encode(responseData)
 }
