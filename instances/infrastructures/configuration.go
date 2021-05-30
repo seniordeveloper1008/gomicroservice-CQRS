4c5df1f421d9c8cf1974bcd6944d437313eff0dc
Senior Dev <seniordev1008@gmail.com>
2021-05-30 04:06:34 -0400

    notify yaml changes

--- instances/infrastructures/configuration.go
+++ instances/infrastructures/configuration.go
@@ -1,6 +1,8 @@
 package infrastructures
 
 import (
+	sLog "log"
+
 	"bitbucket.org/burhanmubarok/microservice/structures/infrastructures"
 	"github.com/fsnotify/fsnotify"
 	config "github.com/spf13/viper"
@@ -8,11 +10,12 @@ import (
 
 // Configuration doc
 type Configuration struct {
-	Name  string
-	Desc  string
-	Env   string
-	Port  int
-	Debug bool
+	Name    string
+	Desc    string
+	Env     string
+	Port    int
+	Debug   bool
+	LogPath string
 }
 
 // Init doc
@@ -33,15 +36,17 @@ func (c *Configuration) set() {
 
 	config.WatchConfig()
 	config.OnConfigChange(func(e fsnotify.Event) {
-		logData := structures.Log{Message: "Configuration file just changed"}
+		logData := structures.Log{Message: "app.yaml just changed"}
 		log := &Logger{}
 		log.Warning(logData)
+		sLog.Println("Warning: app.yaml just changed")
 	})
 	c.Name = config.GetString("microservice.name")
 	c.Desc = config.GetString("microservice.desc")
 	c.Env = config.GetString("microservice.env")
 	c.Port = config.GetInt("microservice.port")
 	c.Debug = config.GetBool("microservice.debug")
+	c.LogPath = config.GetString("microservice.logPath")
 }
 
 // Get doc
