756c74f5adaf58ed5f3f366fce195749df9aebdb
Senior Dev <seniordev1008@gmail.com>
2021-06-25 00:18:44 -0400

    complete log structure

--- structures/infrastructures/log.go
+++ structures/infrastructures/log.go
@@ -2,9 +2,51 @@ package structures
 
 // Log doc
 type Log struct {
-	Message string      `json:"message"`
-	File    string      `json:"file"`
-	Line    string      `json:"line"`
-	Data    interface{} `json:"data"`
-	Errors  interface{} `json:"errors"`
+	Message string `json:"message"`
+}
+
+// LogCommon doc
+type LogCommon struct {
+	Log
+	Data interface{} `json:"data"`
+}
+
+// LogDebug doc
+type LogDebug struct {
+	LogCommon
+}
+
+// LogInfo doc
+type LogInfo struct {
+	LogCommon
+}
+
+// LogWarning doc
+type LogWarning struct {
+	LogCommon
+	Errors interface{} `json:"errors"`
+}
+
+// LogError doc
+type LogError struct {
+	LogCommon
+	File   string      `json:"file"`
+	Line   string      `json:"line"`
+	Errors interface{} `json:"errors"`
+}
+
+// LogFatal doc
+type LogFatal struct {
+	LogCommon
+	File   string      `json:"file"`
+	Line   string      `json:"line"`
+	Errors interface{} `json:"errors"`
+}
+
+// LogPanic doc
+type LogPanic struct {
+	LogCommon
+	File   string      `json:"file"`
+	Line   string      `json:"line"`
+	Errors interface{} `json:"errors"`
 }
