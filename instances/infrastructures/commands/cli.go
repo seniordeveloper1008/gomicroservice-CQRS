624550a3392c1e0d23d98f99e7f872245f94484c
Senior Dev <seniordev1008@gmail.com>
2021-08-05 12:28:02 -0400

    update CLI detail

--- instances/infrastructures/commands/cli.go
+++ instances/infrastructures/commands/cli.go
@@ -8,12 +8,17 @@ import (
 // CLI doc
 type CLI struct{}
 
+// NewCLI doc
+func NewCLI() *CLI {
+	return &CLI{}
+}
+
 // Execute doc
 func (c *CLI) Execute() {
 	cli := &cobra.Command{
 		Use:   "microservice",
-		Short: "Microservice [name]",
-		Long:  "Microservice [desc]",
+		Short: "Microservice <name>",
+		Long:  "Microservice <name>. This microservice will be running under CLI, look at the Usage and Available Commands below for the detail.",
 	}
 
 	var microservice interfaces.Microservice
@@ -23,32 +28,32 @@ func (c *CLI) Execute() {
 		{
 			Use:   "ensure",
 			Short: "Configuration Check",
-			Long:  "a server listener that ran by command line interface",
-			Run: func(command *cobra.Command, args []string) {
+			Long:  "a task that will be ensuring configuration file and it's value.",
+			Run: func(_ *cobra.Command, _ []string) {
 				microservice.Ensure()
 			},
 		},
 		{
 			Use:   "test",
 			Short: "Connection Test",
-			Long:  "a tester to assure that this microservice have a connection and a certain access (database, email, kafka, log, etc)",
-			Run: func(command *cobra.Command, args []string) {
+			Long:  "a task that will assess that all required configuration has been complied and test some of it's module (database, email, kafka, log, etc).",
+			Run: func(_ *cobra.Command, _ []string) {
 				microservice.Test()
 			},
 		},
 		{
 			Use:   "migrate",
 			Short: "Database Migration",
-			Long:  "a server listener that ran by command line interface",
-			Run: func(command *cobra.Command, args []string) {
+			Long:  "a task that will be migrating and seeding database.",
+			Run: func(_ *cobra.Command, _ []string) {
 				microservice.Migrate()
 			},
 		},
 		{
 			Use:   "serve",
 			Short: "Server Listener",
-			Long:  "a server listener that ran by command line interface",
-			Run: func(command *cobra.Command, args []string) {
+			Long:  "a server listener that will be listening any http request.",
+			Run: func(_ *cobra.Command, _ []string) {
 				microservice.Serve()
 			},
 		},
