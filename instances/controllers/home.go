30f318dcf4cfc8c2f4d15cff38a725120d31110a
Senior Dev <seniordev1008@gmail.com>
2021-09-30 01:43:27 -0400

    update var format

--- instances/controllers/home.go
+++ instances/controllers/home.go
@@ -12,5 +12,5 @@ type Home struct{}
 
 // Get doc
 func (h *Home) Get(w http.ResponseWriter, r *http.Request) {
-	new(helpers.Response).SendOK(w, structures.Response{Message: "Wellcome to Microservice [Name]"})
+	new(helpers.Response).SendOK(w, structures.Response{Message: "Wellcome to Microservice <Name>"})
 }
